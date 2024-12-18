a5d418c61a6f1754aa7ec13bf4daed3fba30c02a
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2024-12-18 17:03:30 +0000

    created a start menu method in player class updated player move method to allow access to start menu method

--- lib/player.rb
+++ lib/player.rb
@@ -8,14 +8,25 @@ class Player
 
   def player_move()
     move = 9999
-    until move.between?(1,7)
+    until move == "start" || move.between?(1,7) 
       puts "Enter where you would like to drop your chip?"
       puts "Enter 1-7 only!"
-      move = gets.chomp.to_i
+      move = gets.chomp
+      puts "move is #{move} "
+      if move != "start"
+        move = move.to_i
+        puts "move is an integer #{move.is_a?(Integer)} #{move}"
+      end
     end
-    return move
-    
+    unless move.is_a?(Integer)
+      start_menu()
+    end
   end
-
+  def start_menu
+    puts "puts welcome to the start menu"
+    puts "Enter 1 to resume game\nEnter 2 to save and quit\nEnter 3 start a new game\nEnter 4 to quit"
+    response = gets.chomp
+    puts "you picked #{response} well too bad we\'re quitting sucka!"
+  end
 
 end
